5f537ce6fd6b1132ad0c53359809afee9209ebe9
Forsythe <dforsythe@uiowa.edu>
2018-04-20 02:05:26 -0500

    basic tb

--- andor.srcs/sources_1/imports/Documents/andxor.sv
+++ andor.srcs/sources_1/imports/Documents/andxor.sv
@@ -1,42 +1,62 @@
 `timescale 1ns / 1ps
 
-module andxor ();
+module andxor (
     
-    reg[15:0] X = 16'b0101010101010100;
-    reg[15:0] Y = 16'b1111111111111111;
-    reg[7:0] SUMBIT = 16'b0000000000000000;
-    reg[7:0] r = 8'b0;
-    reg[7:0] in = 8'b00000011;
+    input[15:0] X,
+    input[15:0] Y,
+    output [7:0] SUMBIT,   
+    input[7:0] R,
+    output[7:0] ORING,
+    input CLK, 
+    input ARESET,
+    input InputValid,
+    output InputReady
+    );
     
-    wire[15:0] AND,XOR;
+   
+    assign SUMBIT = (X & Y);
+    assign ORING = ( SUMBIT ^  R);
     
-    integer i;
-    assign AND = X & Y;
+    
+    //possible next implemenatation 
+    
+    /*
+    logic [1:0] state;
+    logic [1:0] nextState;
+    */
+    
+    /*
+    
+   always_ff@(posedge CLK) begin
+                if (ARESET == 1'b0) begin
+                   state <= 2'b00;
+               end else begin
+                   state <= nextState;
+                end
+           end
+   */
+
     
     
-    initial
-        begin
-         //Scalar Tests 
-         $display("AND of X and Y is %b", AND);
-        
-        
-         for(i=0;i<16;i = i+1)begin
-             $display(" AND of each bit in X and Y is %b", AND[i] );
-             SUMBIT = SUMBIT + AND[i];
-         end
-         $display("SUMING together the AND bits will give us %b", SUMBIT);
-         
-         
-         r = in ^ SUMBIT;
-         
-         $display("XORING each sumbit will produce a  output of... %b", r);
-         
-         
-         
-         #100 $finish ;
-        
-          
+    // Next state logic
+    /*    always_comb begin
+            case(state)
+                2'b00 : 
+                    if (InputValid == 1'b1) nextState = 2'b01;
+                    else nextState = 2'b00;
+                2'b01 : nextState = 2'b10;
+                2'b10 : 
+                    if (finish) nextState = 2'b00;
+                    else nextState = 2'b10; 
+            endcase
         end
-  endmodule
+     */   
+    endmodule
+    
+    
+    
+    
+  
+
     
     
